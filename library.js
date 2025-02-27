7b8b3f3584f6833842c1710311c938cbd30e70ac
al-prieto <alejandroprieto.has@gmail.com>
2025-02-26 21:54:58 -0500

    feat: add toggle read status functionality and integrate with checkbox
    - Implement toggleReadStatus method in Book constructor to alternate read status
    - Add checkbox in the library display to toggle read status
    - Ensure proper integration and UI update upon toggling read status
    - Clean up and organize related event handling logic

--- library.js
+++ library.js
@@ -19,23 +19,65 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.toggleReadStatus = function () {
+    this.read = this.read ? false : true;
+    console.log(this.read);
+  };
 }
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
+  newBook.id = myLibrary.length;
   myLibrary.push(newBook);
+}
+
+function removeBook(id) {
+  const bookIndex = myLibrary.findIndex((book) => book.id === id);
+
+  if (bookIndex > -1) {
+    myLibrary.splice(bookIndex, 1);
+  }
+  displayLibrary();
+}
+
+function displayLibrary() {
+  while (body.firstChild) {
+    body.removeChild(body.firstChild);
+  }
+
+  myLibrary.forEach((book) => {
+    const row = body.insertRow();
+
+    row.setAttribute("data-id", book.id);
+    Object.keys(book).forEach((key) => {
+      if (key !== "id" && key !== "read" && key !== "toggleReadStatus") {
+        const cell = row.insertCell();
+        cell.textContent = book[key];
+      }
+    });
+    const checkboxCell = row.insertCell();
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = book.read;
+    checkboxCell.appendChild(checkbox);
+    const btnCell = row.insertCell();
+    const button = document.createElement("button");
+    button.textContent = "Remove";
+    btnCell.appendChild(button);
+
+    checkbox.addEventListener("click", () => {
+      book.toggleReadStatus();
+      displayLibrary();
+    });
+    button.addEventListener("click", () => {
+      removeBook(book.id);
+    });
+  });
 }
 
 addBookToLibrary("El Quijote", "Miguel de Cervantes", 863, true);
 addBookToLibrary("1984", "George Orwell", 328, false);
 
-myLibrary.forEach((book) => {
-  const row = body.insertRow();
-
-  Object.values(book).forEach((value) => {
-    const cell = row.insertCell();
-    cell.textContent = value;
-  });
-});
+displayLibrary();
 
 container.appendChild(table);
